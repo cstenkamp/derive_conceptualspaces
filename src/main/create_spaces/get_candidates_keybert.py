47f3e0b9f06479477440a8b821a6a58c35a3b940
Chris Stenkamp <cstenkamp@gmail.com>
2021-11-25 10:46:23 +0100

    keybert for extraction usable but not too happy with it

--- src/main/create_spaces/get_candidates_keybert.py
+++ src/main/create_spaces/get_candidates_keybert.py
@@ -1,3 +1,4 @@
+import re
 from keybert import KeyBERT
 from nltk.corpus import stopwords as nlstopwords
 
@@ -37,19 +38,42 @@ class KeyBertExtractor():
         #TODO: what if there are special chars in the candidates? is everything ok then with the word-splitting?
         inds, words = tokenize_text(text, stopwords)
         withoutstops = " ".join(words).lower()
+        withoutstopsandcharacters = " ".join([i for i in words if re.compile("[a-zA-Z-]+").fullmatch(i)])
         start_positions = [((start := withoutstops.find(i)), start+len(i)) for i in candidates]
         start_indices_withoutstops = [withoutstops[:i].count(" ") for i,j in start_positions]
-        actual_keyphrases = []
-        for start_ind, (startpos, stoppos) in zip(start_indices_withoutstops, start_positions):
+        actual_keyphrases = [] #TODO ich müsste rehct früh gucken ob zahlen im candidate vorkommen
+        used_candidates = []
+        for start_ind, (startpos, stoppos), cand in zip(start_indices_withoutstops, start_positions, candidates):
             if start_ind >= 0 and startpos >= 0:
                 full_phrase = withoutstops[startpos:stoppos]
-                last_word = full_phrase.split(" ")[-1]
-                from_start = tokenize_text(text.lower(), stopwords)[inds[start_ind]:]
-                try:
-                    actual_phrase = from_start[:from_start.index(last_word)+1]
-                except:
+                if len(full_phrase.split(" ")) != len(set(full_phrase.split(" "))):
+                    print(f"Candidate is weird and doesnt work: {cand}")
+                if len(cand.split(" ")) == 1:
+                    actual_keyphrases.append(cand)
+                    used_candidates.append(cand)
                     continue
+                last_word = full_phrase.split(" ")[-1]
+                try:
+                    words[start_ind:].index(last_word) + 1
+                except ValueError:
+                    print(f"Complete Error at `{cand}`")
+                    continue
+                else:
+                    actual_phrase = words[start_ind:start_ind+words[start_ind:].index(last_word)+1]
+                if " ".join(actual_phrase) != cand: #TODO eigentlich müsste ich gucken ob eins der originalwörter mehfach im substring vorkommt
+                    if len(actual_phrase) > len(cand.split(" "))+2 or (len(cand.split(" ")) > 3 and len(actual_phrase) > len(cand.split(" "))*1.7):
+                        if cand in " ".join(actual_phrase):
+                            actual_phrase = cand.split(" ")
+                            print(f"Original Candidate: `{cand}`, actual candidate: `{' '.join(actual_phrase)}`")
+                            continue
+                    print(f"Original Candidate: `{cand}`, actual candidate: `{' '.join(actual_phrase)}`")
                 if len(actual_phrase) < 10: #TODO parametrizise this value!
                     if " ".join(actual_phrase) in text.lower():
                         actual_keyphrases.append(" ".join(actual_phrase))
-        return actual_keyphrases, candidates
+                        used_candidates.append(cand)
+            elif startpos <= 0 and withoutstopsandcharacters.find(cand) > 0:
+                pass #there is a character in between (teacher ? general didacticts)
+            else:
+                print(f"Candidate didn't work at all: `{cand}`")
+                #TODO wenn in candidate ne zahl oder so ist die entfernen und es neu versuchen
+        return actual_keyphrases, used_candidates
