distance matrix calculation is fixed and also >100 times faster

derive_conceptualspace/cli/create_siddata_dataset.py
@@ -313,29 +313,8 @@ def rank_courses_saldirs(ctx):
     ctx.obj["clusters"] = ctx.obj["json_persister"].load(None, "clusters")
     for desc, embedding in zip(ctx.obj["pp_descriptions"]["descriptions"], list(ctx.obj["mds"].embedding_)):
         desc.embedding = embedding
+    print()
 
-# def rank_courses_saldirs(ctx, pp_descriptions_filename, mds_filename, dcm_filename, clusteredcands_filename):
-#     from derive_conceptualspace.util.base_changer import NDPlane
-#     import numpy as np
-#     ctx.obj["pp_descriptions_filename"] = pp_descriptions_filename
-#     ctx.obj["vocab"], ctx.obj["descriptions"], ctx.obj["pp_components"] = load_preprocessed_descriptions(join(ctx.obj["base_dir"], pp_descriptions_filename))
-#     ctx.obj["mds_filename"] = mds_filename
-#     tmp = json_load(join(ctx.obj["base_dir"], mds_filename))
-#     assert tmp["pp_components"] == ctx.obj["pp_components"]
-#     print(f"Using the MDS which had {tmp['quant_measure']} as quantification measure!")
-#     mds = tmp["mds"]
-#     dcm = DocTermMatrix(json_load(join(ctx.obj["base_dir"], dcm_filename), assert_meta=("CANDIDATE_MIN_TERM_COUNT", "STANFORDNLP_VERSION")))
-#     descriptions = ctx.obj["descriptions"]
-#     assert len(dcm.dtm) == len(descriptions), f"The Doc-Candidate-Matrix contains {len(dcm.dtm)} items and you have {len(descriptions)} descriptions!"
-#     for desc, embedding in zip(descriptions, list(mds.embedding_)):
-#         desc.embedding = embedding
-#     tmp = json_load(join(ctx.obj["base_dir"], clusteredcands_filename))
-#     clusters, cluster_directions, kappa_scores, decision_planes = tmp["clusters"], tmp["cluster_directions"], tmp["kappa_scores"], tmp["decision_planes"]
-#     clusters = {k: {"components": [k]+v, "direction": cluster_directions[k], "kappa_scores": {i: kappa_scores[i] for i in [k]+v}, "decision_planes": {i: NDPlane(np.array(decision_planes[i][1]), decision_planes[i][0]) for i in [k]+v}} for k,v in clusters.items()}
-#
-#
-
-#
 #
 # @prepare_candidateterms.command()
 # @click.pass_context

derive_conceptualspace/create_spaces/create_mds.py
@@ -6,11 +6,19 @@ import numpy as np
 from tqdm import tqdm
 from sklearn.manifold import MDS
 import scipy.sparse.csr
+from scipy.spatial.distance import pdist, squareform, cdist
 
 from derive_conceptualspace.settings import get_setting
 
 logger = logging.getLogger(basename(__file__))
 
+
+norm_ang_diff = lambda v1, v2: 2/math.pi * np.arccos(np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2)))
+cos_to_normangdiff = lambda cosine: 2/math.pi*(np.arccos(-cosine+1))
+# https://stackoverflow.com/questions/35758612/most-efficient-way-to-construct-similarity-matrix
+# https://www.kaggle.com/cpmpml/ultra-fast-distance-matrix-computation/notebook
+# https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.distance.cosine.html -> it's 2/math.pi*(np.arccos(-cosine+1)
+# it WOULD be squareform(np.apply_along_axis(cos_to_normangdiff, 0, pdist(arr, metric="cosine"))), but this way we don't have a progressbar
 
 def create_dissimilarity_matrix(arr, full=False):
     """returns the dissimilarity matrix, needed as input for the MDS. Input is the dataframe
@@ -24,22 +32,32 @@ def create_dissimilarity_matrix(arr, full=False):
     assert arr.shape[0] < arr.shape[1], "I cannot believe your Doc-Term-Matrix has less distinct words then documents."
     assert arr.max(axis=1).min() > 0, "If one of the vectors is zero the calculation will fail!"
     logger.info("Creating the dissimilarity matrix...")
-    res = np.zeros((arr.shape[0],arr.shape[0]))
-    with tqdm(total=round(((arr.shape[0]*arr.shape[0])-arr.shape[0])*(1 if full else 0.5))) as pbar:
-        for n1, e1 in enumerate(arr):
-            for n2, e2 in enumerate(arr):
-                if not full and n2 < n1:
-                    continue
-                if n1 != n2:
-                    p1 = np.dot(e1, e2) / (np.linalg.norm(e1) * np.linalg.norm(e2))
-                    if 0 < p1-1 < 1e-12:
-                        p1 = 1 #aufgrund von rundungsfehlern kann es >1 sein
-                    res[n1,n2] = 2 / math.pi * math.acos(p1)
-                    pbar.update(1)
-    if not full:
-        res[res.T > 0] = res.T[res.T > 0]
-    assert np.allclose(res, res.T, atol=1e-10)
+    # return squareform(np.apply_along_axis(cos_to_normangdiff, 0, pdist(arr, metric="cosine")))
+    # assert np.allclose(np.hstack([cdist(arr, arr[i*10:(i+1)*10], "cosine") for i in range(10)]), squareform(tmp))
+    tmp = []
+    N_CHUNKS = 200
+    for chunk in tqdm(np.array_split(arr, N_CHUNKS)):
+        tmp.append(cdist(arr, chunk, "cosine"))
+    assert np.allclose(np.hstack(tmp), np.hstack(tmp).T), "The matrix must be symmetric!"
+    flat = squareform(np.hstack(tmp), checks=False) #I check in the line above, dunno why this one fails if the upper works..
+    res = squareform(np.apply_along_axis(cos_to_normangdiff, 0, flat))
     return res
+    # res = np.zeros((arr.shape[0],arr.shape[0]))
+    # with tqdm(total=round(((arr.shape[0]*arr.shape[0])-arr.shape[0])*(1 if full else 0.5))) as pbar:
+    #     for n1, e1 in enumerate(arr):
+    #         for n2, e2 in enumerate(arr):
+    #             if not full and n2 < n1:
+    #                 continue
+    #             if n1 != n2:
+    #                 p1 = np.dot(e1, e2) / (np.linalg.norm(e1) * np.linalg.norm(e2))
+    #                 if 0 < p1-1 < 1e-12:
+    #                     p1 = 1 #aufgrund von rundungsfehlern kann es >1 sein
+    #                 res[n1,n2] = 2 / math.pi * math.acos(p1)
+    #                 pbar.update(1)
+    # if not full:
+    #     res[res.T > 0] = res.T[res.T > 0]
+    # assert np.allclose(res, res.T, atol=1e-10)
+    # return res
 
 
 def create_mds(dissim_mat, mds_dimensions):

derive_conceptualspace/load_data/load_semanticspaces.py
@@ -4,6 +4,9 @@ import json
 
 import numpy as np
 
+from derive_conceptualspace.util.base_changer import ThreeDPlane
+from derive_conceptualspace.util.threedfigure import ThreeDFigure, make_meshgrid
+
 flatten = lambda l: [item for sublist in l for item in sublist]
 
 TRANSLATE_FNAME = {"movies": "films"}
@@ -11,6 +14,27 @@ TRANSLATE_FNAME = {"movies": "films"}
 #TODO If I want to do that for classes as well, I have to create these as well, not only load
 #TODO Do this ones also for the [ALBS20] and [AGKS18] datasets
 data_base, data_set, n_dims = "/home/chris/Documents/UNI_neu/Masterarbeit/data/semanticspaces/", "movies", 20
+
+
+def main():
+    canditerms, cluster_directions, mds_class_dict = get_all()
+    three_dims = list(cluster_directions.keys())[:3]
+    entities = {k: (v[1], np.array([v[2][k2] for k2 in three_dims])) for k, v in mds_class_dict.items()}
+    display_svm(entities, {k: cluster_directions[k] for k in three_dims})
+
+
+def display_svm(entities, cluster_directions):
+    X = np.array([i[1] for i in entities.values()])
+    assert X.shape[1] == 3
+    extras = [{"Name": i[0], "Classes": i[1][0]} for i in list(entities.items())]
+    with ThreeDFigure(name=",".join(cluster_directions.keys())) as fig:
+        fig.add_markers(X, color="blue", custom_data=extras, name="samples")
+        for num, (dirname, sw_ax, color) in enumerate(zip(cluster_directions.keys(), ["xz", "yz", None], ["red", "green", "yellow"])):
+            # fig.add_surface(ThreeDPlane(np.eye(3)[num], 0), X, color="gray")
+            fig.add_surface_old(*make_meshgrid(size=0.2), lambda _, __: 0, opacity=0.3, showlegend=True, color=color, name=dirname, swap_axes=sw_ax)
+        fig.add_markers([0, 0, 0], size=3, name="Coordinate Center")  # coordinate center
+        fig.show()
+
 
 def get_all():
     feat_vecs = load_ppmi_weighted_feature_vectors(data_base, data_set)            #./Tokens/*  & Tokens.json
@@ -32,14 +56,16 @@ def get_all():
     proj1, proj2 = load_projections(data_base, data_set, n_dims)                   #./dXX/filmsXX.projected and ./dXX/projectionsXX.data
     assert all(any(np.allclose(i, proj) for i in cluster_directions.values()) for proj in proj1)
     #proj1: 40*20 (same as cluster_directions), proj2: 15000*40
+    #proj2 are the distances to the origins of the respective dimensions (induced by the clusters), what induces the respective rankings! (see DESC15 p.24u)
     mds_dict = dict(zip(names, list(mds)))
     assert mds_dict.keys() == classes.keys()
-    #soo let's use all data. For every movie, there's a something in `names`, `mds`, `classes` and `proj2` (whatever that is) => `mds_class_dict`
+    #soo let's use all data. For every movie, there's a something in `names`, `mds`, `classes` and `proj2` => `mds_class_dict`
     #`canditerms` uses the `clusters`, `proj1` is the same as `cluster_directions`
     #missing: `feat_vecs` (idk how to map these to names), `all_terms` (extracted phrases are ngrams),
     mds_class_dict = dict(zip(mds_dict.keys(), list(zip(mds_dict.values(), classes.values(), proj2))))
+    mds_class_dict = {k: (v[0], v[1], dict(zip(cluster_directions.keys(), v[2]))) for k,v in mds_class_dict.items()}
     return canditerms, cluster_directions, mds_class_dict
-
+    #ok, in spirit of DESC15 I want a class that can find min and max and compare entities w.r.t. different dimensions
 
 
 def load_projections(data_base, data_set, n_dims):

derive_conceptualspace/util/base_changer.py
@@ -35,8 +35,11 @@ class ThreeDPlane(NDPlane):
 
 def make_base_changer(plane: ThreeDPlane):
     uvec1 = plane.normal
-    uvec2 = [0, -plane.d / plane.b, plane.d / plane.c]  # NOT [1, 0, plane.z(model, 1, 0)] !!
-    uvec3 = np.cross(uvec1, uvec2)
+    if plane.intercept == 0 and len([i for i in plane.normal if i != 0]) == 1:
+        uvec2, uvec3 = [(np.eye(3)*[i for i in plane.normal if i != 0][0])[i] for i in set(range(3))-set([np.argmax(plane.normal)])]
+    else:
+        uvec2 = [0, -plane.d / plane.b, plane.d / plane.c]  # NOT [1, 0, plane.z(model, 1, 0)] !!
+        uvec3 = np.cross(uvec1, uvec2)
     transition_matrix = np.linalg.inv(np.array([uvec1, uvec2, uvec3]).T)
 
     origin = np.array([0, 0, 0])

derive_conceptualspace/util/threedfigure.py
@@ -62,16 +62,16 @@ class ThreeDFigure():
         points[:, ind2] = tmp
         return points
 
-    def _get_meshgrid(self, plane, samples, labels, margin):
+    def _get_meshgrid(self, plane, samples, margin):
         trafo, back_trafo = make_base_changer(plane)
-        onto_plane = np.array([back_trafo([0, trafo(point)[1], trafo(point)[2]]) for point, side in zip(samples, labels)])
+        onto_plane = np.array([back_trafo([0, trafo(point)[1], trafo(point)[2]]) for point in samples])
         minx, miny, minz = onto_plane.min(axis=0)
         maxx, maxy, maxz = onto_plane.max(axis=0)
         xx, yy = make_meshgrid(minx=minx, miny=miny, maxx=maxx, maxy=maxy, margin=margin)
         return xx, yy, minz, maxz
 
-    def _get_surface_tight(self, plane, samples, labels, margin):
-        xx, yy, minz, maxz = self._get_meshgrid(plane, samples, labels, margin)
+    def _get_surface_tight(self, plane, samples, margin):
+        xx, yy, minz, maxz = self._get_meshgrid(plane, samples, margin)
         xy_arr = np.vstack([xx.flatten(), yy.flatten()]).T
         col_fn = lambda x, y: 0 if minz - margin < plane.z(np.array([x,y])) < maxz + margin else 1
         z_fn = lambda x, y: min(max(plane.z(np.array([x, y])), minz - margin), maxz + margin)
@@ -82,8 +82,8 @@ class ThreeDFigure():
         res = {k: surface_form(points[:, ind]) for ind, k in enumerate("xyz")}
         return {**res, **{"surfacecolor": surface_form(cols)}}
 
-    def add_surface(self, plane, samples, labels, margin=0, swap_axes=None, opacity=0.9, color="blue", showlegend=False):
-        xx, yy, zz, cols = [v for v in self._get_surface_tight(plane, samples, labels, margin).values()]
+    def add_surface(self, plane, samples, labels=None, margin=0, swap_axes=None, opacity=0.9, color="blue", showlegend=False):
+        xx, yy, zz, cols = [v for v in self._get_surface_tight(plane, samples, margin).values()]
         points = np.vstack([xx.flatten(), yy.flatten(), zz.flatten()]).T
         points = self._transform(points)
         if swap_axes:
@@ -101,7 +101,7 @@ class ThreeDFigure():
                        )
         )
 
-    def add_surface_old(self, xx, yy, z_func, swap_axes=None, opacity=None): #TODO merge with ^
+    def add_surface_old(self, xx, yy, z_func, swap_axes=None, opacity=None, color=None, showlegend=False, name=None): #TODO merge with ^
         xy_arr = np.vstack([xx.flatten(), yy.flatten()]).T
         z_arr = np.array([z_func(*i) for i in xy_arr])
         points = np.column_stack([xy_arr, z_arr])
@@ -109,9 +109,11 @@ class ThreeDFigure():
         if swap_axes:
             points = self._swap_axes(points, swap_axes)
         surface_form = lambda x: x.reshape(round(math.sqrt(x.shape[0])), -1)
+        kwargs = dict(colorscale=[[0, color], [1, color]]) if color is not None else {}
         self.fig.add_trace(
             go.Surface(x=surface_form(points[:, 0]), y=surface_form(points[:, 1]), z=surface_form(points[:, 2]),
-                       opacity=opacity, showlegend=False, showscale=False))
+                       opacity=opacity, showlegend=showlegend, showscale=False, name=name, **kwargs))
+
 
     def add_line(self, point1, point2, width=6, do_transform=True, name=None, **kwargs):
         if do_transform: